ffbf24e213ce52a5aab74fc207fcd5240ea8a9cd
odm275 <pomejia@gmail.com>
2018-01-14 17:28:18 -0600

    It is spagetti, but the core functionality works

--- js/index.js
+++ js/index.js
@@ -23,141 +23,131 @@ var handleState = {
     clearState: function(){
         this.state = [];
     },
-    random: function(){
-        //this.state.push(Math.floor((Math.random() * 4)));
-        switch(Math.floor((Math.random() * 4))){
-            case 0:
-            this.state.push('green');
-            break;
-            case 1:
-            this.state.push('pink');
-            break;
-            case 2:
-            this.state.push('orange');
-            break;
-            case 3:
-            this.state.push('blue');
-            break;
-        }
-    },
 };
 
 var state = Object.create(handleState);
-//Some combination of root behavior
+//Some combination of root behavior, we can reduce
+//stuff going on humanTurn/Computer Turn from here
 
+////////////////////////////////
+//STUFF TO REDUCE/COMBINE handleState methods.
+///////////////////////////////
 
+var computer = Object.create(state);
+var human = Object.create(state);
 
-state.computerTurn = function(){
-    var self = this;
-    this.random();
+var match = function(humanState, computerState){
+//See if human matched computer's array.
+    for(var i = 0; i < humanState.length; i++){
+        if (humanState[i] !== computerState[i]){
+            return false;
+        }
+    }
+    return true;
+}
+//Exclusive object(s) for human:
+human.humanTurn = function(btn){
+    human.state.push(btn);
+    console.log('HUMAN STATE:')
+    console.log(human.state);
 
-for(var i = 0; i < this.getState().length;i++){
+    var check = match(human.state, computer.state); //Check if human matched computer.
+    console.log("The match is: " + check);
+    //Constaint:
+    //Once we have enough pushes to at least have a case to compare the arrays
+    if(check && (human.state.length === computer.state.length) ){//1
+        human.clearState();//Continue and add n+1 state
+            setTimeout(function timer(){
+            computer.computerTurn(check);        
+        },3000);
+    } else if(!check && human.strict){//3
+        //  STRICT MODE: ON
+        computer.clearState();
+        human.clearState();
+        computer.computerTurn();//Start over
+    } else if(!check && !human.strict){
+        human.clearState();//Continue and add n+1 state
+        setTimeout(function timer(){
+        computer.computerTurn(check);        
+        },3000);
+    }
+};
+
+//Exclusive object(s) for computer:
+computer.random = function(){
+    switch(Math.floor((Math.random() * 4))){
+        case 0:
+        this.state.push('green');
+        break;
+        case 1:
+        this.state.push('pink');
+        break;
+        case 2:
+        this.state.push('orange');
+        break;
+        case 3:
+        this.state.push('blue');
+        break;
+    }
+}
+computer.computerTurn = function(check){
+    if(check || computer.counter === 0){
+        computer.random();            
+    }
+    
+    for(var i = 0; i < computer.getState().length;i++){
     (function(j){
         setTimeout(function timer(){
             //The last iteration will stll be on here.
-            
-            var color = self.getState()[j];
-            var button = self.getButtons()[color];
+            var color = computer.getState()[j];
+            var button = computer.getButtons()[color];
             $(button).toggleClass("btn--computer"+color);
-
             setTimeout(function timer(){
                 $(button).toggleClass("btn--computer"+color);
             }, 2000);
-        },j*2000);
-        
+        },j*3000);
     })(i);
 }
+console.log("COMPUTER STATE:")
+console.log(computer.state);
+console.log(computer.counter);
+if(computer.counter + 1 ===computer.state.length){
+    computer.counter++;
+}
 
-console.log(this.getState());
 };
-
-state.humanTurn = function(btn){
-        console.log(this.getState());
-
-        
-};
-
 
 $(document).ready(function(){
-var computer = Object.create(state);
-var human = Object.create(state);
+
 //Set the Initial Conditions
 computer.setState([]);
 computer.setButtons();
 human.setState([]);
 human.setButtons();
-  
+human.strict = false;
+computer.counter = 0;
+
   //ON BUTTON
 $(".start").click(function(){
+    
     computer.computerTurn();
+});
+$(".strict").click(function(){
+    human.strict = !human.strict;
 });
 
 $(human.getButtons().green).click(function(){
-    human.humanTurn(this);
+    human.humanTurn('green');
     //human.humanTurn();
 });
 $(human.getButtons().pink).click(function(){
-    human.humanTurn(this);
+    human.humanTurn('pink');
 });
 $(human.getButtons().orange).click(function(){
-    human.humanTurn(this);  
+    human.humanTurn('orange');  
 });
 $(human.getButtons().blue).click(function(){
-    human.humanTurn(this);
+    human.humanTurn('blue');
 });
-
-
 
 })
-
-
-//Computer's turn
-/*simon.newState({
-    green: $(".btn--green").on("click", function(){
-       console.log(this);
-    }),
-    pink: $(".btn--pink").click(function(){
-        console.log(this);
-    }),
-    orange: $(".
-+   btn--orange").click(function(){
-        console.log(this);
-    }),
-    blue: $(".btn--blue").click(function(){
-        console.log(this);
-    })
-});
-*/
-
-;
-
-
-
-//----------------------------------------------------------------------//
-/*
-var Widget = {
-    clickOn: function(btn){
-        this.$elem = null;
-    },
-    insert: function($where){
-        if(this.$elem){
-            this.$elem.css({
-                width: this.width + "px",
-                height: this.height + "px"
-            }).appendTo( $where );
-        }
-    }
-};
-
-var Button = Object.create( Widget );
-
-Button.setup = function(btn,btn2,btn3,btn4){
-    //delegated call
-    this.linkButton(btn,btn1,btn2,btn3);
-};
-
-Buttton.onClick = function(evt) {
-    console.log("Button " + this.label + "clicked!")
-}
-
-*/
